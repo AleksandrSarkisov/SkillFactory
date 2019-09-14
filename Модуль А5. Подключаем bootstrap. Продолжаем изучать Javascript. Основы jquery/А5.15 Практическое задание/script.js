ce50d7118500657adf49fd38181a7afb932501a4
sasha <sark1996@mail.ru>
2019-09-14 20:54:58 +0300

    finish A5 Practice

--- Модуль А5. Подключаем bootstrap. Продолжаем изучать Javascript. Основы jquery/А5.15 Практическое задание/script.js
+++ Модуль А5. Подключаем bootstrap. Продолжаем изучать Javascript. Основы jquery/А5.15 Практическое задание/script.js
@@ -1,36 +1,43 @@
+function replace(str, userInput){
+  mask = /{\w+}/g;
+  match = str.match(mask);
+  for (let i = 0; i < match.length; i++){
+    str = str.replace(match[i], "<input placeholder="+match[i]+">");
+  }
+  return str;
+}
+
+function story (data){
+  inputs = []
+  mask = /{\w+}/g;
+  for (let i=0; i<data['text'].length; i++){
+    match = data['text'][i].match(mask)
+    for (let j = 0; j<match.length; j++){
+      if (!inputs.includes(match[j])){
+        inputs.push(match[j]);
+      }
+    }
+  }
+  for (let i = 0; i<inputs.length; i++){
+    html = '<input name="'+inputs[i]+'" style="margin-bottom: 10px; width: 200px;" placeholder="'+inputs[i]+'">';
+    $('form div').append(html);
+  }
+}
+
+
 $(document).ready(function(){
-
-  function replace(str){
-    mask = /{\w+}/g;
-    match = str.match(mask);
-    for (let i = 0; i < match.length; i++){
-      str = str.replace(match[i], "<input placeholder="+match[i]+">");
-    }
-    return str;
-  }
-
-  const story = data => {
-
-    for (let i=0; i<data['text'].length; i++){
-      data['text'][i] = replace(data['text'][i]);
-    }
-
-    const $html = $(`
-    <div class="container">
-      <div class="row">
-        <div class="story">
-          <h2>Текст</h2>
-          <p>${data.text}</p>
-        </div>
-      </div>
-    </div>
-    `)
-
-    $html.appendTo(document.body);
-  }
-
   $.getJSON('https://api.myjson.com/bins/jcmhn', function(data){
     story(data);
-    console.log(data);
+    $('button').click(function(){
+      mask = /{\w+}/g;
+      for (let i=0;i<data['text'].length;i++){
+        match = data['text'][i].match(mask);
+        for (let j=0;j<match.length;j++){
+          data['text'][i] = data['text'][i].replace(match[j], $('input[name="'+match[j]+'"]')[0].value);
+        }
+      }
+      $('.result').append('<p>'+data['text']+'</p>');
+    });
   });
+
 });
