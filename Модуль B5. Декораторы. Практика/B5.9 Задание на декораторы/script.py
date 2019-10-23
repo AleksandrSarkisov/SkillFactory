fd19b317664961a9a0ed09fef57860339b10a588
A S <u1114@office.local>
2019-10-23 16:53:25 +0300

    reorganise B5 module

--- Модуль B5. Декораторы. Практика/B5.9 Задание на декораторы/script.py
+++ Модуль B5. Декораторы. Практика/B5.9 Задание на декораторы/script.py
@@ -1,35 +1,21 @@
 import time
 
-class timer():
-    avg_time = 0
+def time_this(num_runs=10):
+    time_lists = []
+    def func_wrap(fun):
+        def f():
+            for _ in range(num_runs):
+                start = time.time()
+                fun()
+                time_lists.append(time.time()-start)
+            return sum(time_lists)/len(time_lists)
+        return f
+    return func_wrap
 
-    def __call__(self, fun):
-        def time_this(num_runs=10):
-            time_list = []
-            def func_wrap(fun):
-                def f():
-                    for _ in range(num_runs):
-                        start = time.time()
-                        fun()
-                        time_list.append(time.time()-start)
-                        self.avg_time = sum(time_list)/len(time_list)
-                        return self.avg_time
-                return f
-            return func_wrap
 
-obj = timer()
+@time_this(num_runs=10)
 def f():
     for j in range(1000000):
         pass
 
-print(obj(f()))
-
-def f1():
-    l = []
-    for i in range(10):
-        start = time.time()
-        for j in range(1000000):
-            pass
-        l.append(time.time()-start)
-    return sum(l)/len(l)
-print(f1())
+print(f())
